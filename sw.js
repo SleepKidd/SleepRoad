fb05e06f60d7120f13c51efa2195ae70df7dc1b2
SleepKidd <bebrochkacotika@gmail.com>
2026-09-15 17:49:34 +0500

    Refine models course and world motion

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE='sleep-road-v8';
+const CACHE='sleep-road-v9';
 const ASSETS=['./','./index.html','./style.css','./engine.js','./systems-v4.js','./gameplay-v4.js','./render-v4.js','./boot-v4.js','./icon.svg','./manifest.webmanifest'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
