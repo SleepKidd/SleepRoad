2e6c195bdd7647f16c31f1b0bc0d0479bf450732
Sleepkidd <bebrochkacotika@gmail.com>
2026-09-18 18:32:31 +0500

    Add converted uploaded car model metadata

---
+++ assets/models/car-model-meta.js
@@ -0,0 +1,3 @@
+'use strict';
+// Web conversion of uploaded 027.max. Source SHA-256: 543e5ef2b94bb1a6f9514fdfb45d92b9838d9f59583385804c3effbf1a541c64
+window.SleepRoadCarModelMeta={source:'027.max',sourceSha256:'543e5ef2b94bb1a6f9514fdfb45d92b9838d9f59583385804c3effbf1a541c64',bounds:{min:[-.93,0,-2.09],max:[.93,1.44,2.09]}};
