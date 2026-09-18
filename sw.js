4753fc0ac27cea5377f68a719a176bc79f0e84d6
Sleepkidd <bebrochkacotika@gmail.com>
2026-09-18 11:27:52 +0500

    Cache Level Director v5 assets

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
-const CACHE='sleep-road-v12';
-const ASSETS=['./','./index.html','./style.css','./engine.js','./assets/models/countmaster-character.js','./systems-v4.js','./gameplay-v4.js','./render-v4.js','./boot-v4.js','./icon.svg','./manifest.webmanifest'];
+const CACHE='sleep-road-v13';
+const ASSETS=['./','./index.html','./style.css','./engine.js','./assets/models/countmaster-character.js','./systems-v4.js','./level-director-v5.js','./gameplay-v4.js','./level-runtime-v5.js','./render-v4.js','./render-levels-v5.js','./boot-v4.js','./icon.svg','./manifest.webmanifest'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',event=>{
